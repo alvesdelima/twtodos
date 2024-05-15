dba218026068c1b731ce9f2c7bf2d5c65baa08a6
alvesdelima'git commit -m versão <alvesdeliima@gmail.com>
2024-05-15 10:41:07 -0300

    Rename home to index

--- app_todos/views.py
+++ app_todos/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'app_todos/home.html')
+    return render(request, 'app_todos/index.html')
